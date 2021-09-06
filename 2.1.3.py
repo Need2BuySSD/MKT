e8c8caded101ea0f597138a9db1dfd8edb7b34da
Need2BuySSD <89660772+Need2BuySSD@users.noreply.github.com>
2021-09-07 00:54:37 +0300

    Add files via upload

--- 2.1.3.py
+++ 2.1.3.py
@@ -6,8 +6,8 @@ p1 = (4, '$p_0$')
 p2 = (8, '$4p_0$')
 v1 = (4, '$V_0$')
 v2 = (8, '$2V_0$')
-state1 = State(axes, p=p2, v=v1, patch='1', patch_pos='T')
-state2 = State(axes, p=p1, v=v2, patch='2', patch_pos='R')
+state1 = State(axes, p=p2, v=v1, patch='1', patch_pos='TT')
+state2 = State(axes, p=p1, v=v2, patch='2', patch_pos='RR')
 
 axes.process(state1,state2, arrow=False)
 
@@ -19,5 +19,6 @@ axes.line(state2, 'xp')
 axes.line(state1, 'yp')
 axes.line(state2, 'yp')
 
-#savefig('2.1.3.pdf')
+savefig('2.1.3.pdf')
+
 show()
